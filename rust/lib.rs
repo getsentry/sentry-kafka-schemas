59783c4025d43132c6124d327ead1d074efb4f99
Lyn <lyn.nagara@gmail.com>
2023-03-02 16:49:51 -0800

    feat: Implement `get_schema` in Rust
    Basic rust implementation. Doesn't handle schema versions or parsing
    json schemas yet.

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,8 +1,92 @@
-#[derive(PartialEq, Debug)]
-pub struct Schema {}
+use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::fs::{read_to_string, File};
 
-pub fn get_schema(topic: &str, version: Option<u16>) -> Option<Schema> {
-    None
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[non_exhaustive]
+enum SchemaType {
+    #[serde(rename = "json")]
+    Json,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[non_exhaustive]
+enum CompatibilityMode {
+    #[serde(rename = "none")]
+    None,
+    #[serde(rename = "backward")]
+    Backward,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+#[non_exhaustive]
+pub enum SchemaError {
+    TopicNotFound,
+    InvalidVersion,
+    InvalidType,
+}
+
+impl fmt::Display for SchemaError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            SchemaError::TopicNotFound => write!(f, "Topic not found"),
+            SchemaError::InvalidVersion => write!(f, "Invalid version"),
+            SchemaError::InvalidType => write!(f, "Invalid type"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct TopicSchema {
+    version: u16,
+    #[serde(rename = "type")]
+    schema_type: SchemaType,
+    compatibility_mode: CompatibilityMode,
+    resource: String,
+    examples: Vec<String>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct TopicData {
+    topic: String,
+    schemas: Vec<TopicSchema>,
+}
+
+impl TopicData {
+    fn load(path: &str) -> Result<Self, SchemaError> {
+        let f = File::open(path).map_err(|_| SchemaError::TopicNotFound)?;
+        serde_yaml::from_reader(f).map_err(|_| SchemaError::TopicNotFound)
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Schema {
+    version: u16,
+    schema_type: SchemaType,
+    compatibility_mode: CompatibilityMode,
+    // TODO: Actually parse the json schema
+    schema: String,
+}
+
+pub fn get_schema(topic: &str, _version: Option<u16>) -> Result<Schema, SchemaError> {
+    let topic_path = format!("./topics/{}.yaml", topic);
+    let topic_data = TopicData::load(&topic_path)?;
+    // TODO: Respect version
+    let latest = topic_data.schemas.last().unwrap();
+    if latest.schema_type != SchemaType::Json {
+        return Err(SchemaError::InvalidType);
+    }
+
+    let json_schema_path = format!("./schemas/{}", latest.resource);
+
+    Ok({
+        Schema {
+            version: latest.version,
+            schema_type: latest.schema_type.clone(),
+            compatibility_mode: latest.compatibility_mode.clone(),
+            schema: read_to_string(json_schema_path).unwrap(),
+        }
+    })
 }
 
 #[cfg(test)]
@@ -11,6 +95,9 @@ mod tests {
 
     #[test]
     fn test_get_schema() {
-        assert_eq!(get_schema("asdf", None), None);
+        assert_eq!(get_schema("asdf", None), Err(SchemaError::TopicNotFound));
+        let schema = get_schema("querylog", None).unwrap();
+        assert_eq!(schema.version, 1);
+        assert_eq!(schema.schema_type, SchemaType::Json);
     }
 }
