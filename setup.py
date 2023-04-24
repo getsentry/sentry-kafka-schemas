b2a8be8c61036da1dad46fdc1662dd6ca66eab4c
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2023-04-24 15:11:41 +0200

    ref: Move to pyproject.toml
    WIP of moving to pyproject.toml so I can use https://github.com/mitsuhiko/rye
    
    Whether that's a good idea is TBD -- semi-sure it will break craft

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-from typing import Sequence
-
-
-def get_requirements() -> Sequence[str]:
-    with open("python/requirements.txt") as fp:
-        return [x.strip() for x in fp if not x.startswith("#")]
-
-setup(
-    name="sentry-kafka-schemas",
-    version="0.0.29",
-    author="Sentry",
-    author_email="oss@sentry.io",
-    url="https://github.com/getsentry/sentry-kafka-schemas",
-    description="Kafka topics and schemas for Sentry",
-    zip_safe=False,
-    install_requires=get_requirements(),
-    packages=["sentry_kafka_schemas", "sentry_kafka_schemas.schema_types"],
-    package_dir={"": "python/"},
-    package_data={"sentry_kafka_schemas": ["py.typed"]},
-    include_package_data=True,
-    options={"bdist_wheel": {"universal": "1"}},
-)
+setup()
