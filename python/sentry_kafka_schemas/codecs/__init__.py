5c2728a3586efb2007e1631e4e603382f52cc3b8
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2023-05-03 22:20:32 +0200

    fix: Make Codec.validate public (#124)
    I didn't realize the entire time that that method was not on the
    baseclass. It is useful because currently we are treating validation
    errors as just warnings, meaning that we need to decode even if
    validation fails.
    
    We already effectively used arroyo codecs this way.

--- python/sentry_kafka_schemas/codecs/__init__.py
+++ python/sentry_kafka_schemas/codecs/__init__.py
@@ -21,6 +21,17 @@ class Codec(ABC, Generic[T]):
         """
         raise NotImplementedError
 
+    @abstractmethod
+    def validate(self, data: T) -> None:
+        """
+
+        Validate a decoded Python object again.
+
+        Calling `decode(validate=False)` and then `validate()` should be
+        equivalent to `decode(validate=True)`.
+        """
+        raise NotImplementedError
+
 
 class ValidationError(Exception):
     """
