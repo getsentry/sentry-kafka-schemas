aae0dc407eb637fa2f1821f4cae9b614405436ed
Lyn Nagara <lyn.nagara@gmail.com>
2024-02-15 11:58:35 -0800

    feat: Export list_topics (#218)
    So it can be used to create all topics, and ensure they stay in sync across various projects

--- python/sentry_kafka_schemas/__init__.py
+++ python/sentry_kafka_schemas/__init__.py
@@ -3,6 +3,7 @@ from sentry_kafka_schemas.sentry_kafka_schemas import (
     SchemaNotFound,
     get_codec,
     get_topic,
+    list_topics,
 )
 
-__all__ = ["get_codec", "iter_examples", "SchemaNotFound", "get_topic"]
+__all__ = ["get_codec", "iter_examples", "SchemaNotFound", "get_topic", "list_topics"]
