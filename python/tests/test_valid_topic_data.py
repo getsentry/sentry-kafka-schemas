af2d9ebdbaca914ac8f0d189765e2f1fcb421cc9
Lyn Nagara <lynnagara@users.noreply.github.com>
2024-06-27 17:57:38 -0700

    fix snuba topics too

--- python/tests/test_valid_topic_data.py
+++ python/tests/test_valid_topic_data.py
@@ -167,25 +167,31 @@ def test_retention() -> None:
 
 def test_dlq_configuration() -> None:
     # This file does not match the naming convention
-    custom_dlq_mapping = {"ingest-generic-metrics-dlq": "ingest-performance-metrics"}
+
+    custom_dlq_mapping = {
+        "ingest-generic-metrics-dlq": "ingest-performance-metrics",
+        "snuba-dead-letter-replays": "ingest-replay-events",
+        "snuba-dead-letter-metrics": "snuba-metrics",
+        "snuba-dead-letter-querylog": "snuba-queries",
+        "snuba-dead-letter-generic-metrics": "snuba-generic-metrics",
+    }
 
     topics_dir = _TOPICS
     for filename in topics_dir.iterdir():
         if filename.stem.endswith("-dlq"):
             main_topic_name = custom_dlq_mapping.get(filename.stem, filename.stem[:-4])
-            with (
-                open(filename) as dlq_file,
-                open(f"{filename.parent}/{main_topic_name}.yaml") as main_topic_file,
-            ):
-                dlq_topic_data = safe_load(dlq_file)
-                main_topic_data = safe_load(main_topic_file)
 
-                # max.message.bytes matches
-                assert dlq_topic_data["topic_creation_config"].get(
-                    "max.message.bytes"
-                ) == main_topic_data["topic_creation_config"].get("max.message.bytes")
+        elif filename.stem.startswith("snuba-dead-letter-"):
+            main_topic_name = custom_dlq_mapping.get(filename.stem, filename.stem[18:])
+        else:
+            continue
 
-                # DLQ has 7 day retention
-                assert dlq_topic_data["topic_creation_config"]["retention.ms"] == str(
-                    7 * 1000 * 60 * 60 * 24
-                )
+        with open(filename) as dlq_file, open(f"{filename.parent}/{main_topic_name}.yaml") as main_topic_file:
+            dlq_topic_data = safe_load(dlq_file)
+            main_topic_data = safe_load(main_topic_file)
+
+            # max.message.bytes matches
+            assert dlq_topic_data["topic_creation_config"].get("max.message.bytes") == main_topic_data["topic_creation_config"].get("max.message.bytes")
+
+            # DLQ has 7 day retention
+            assert dlq_topic_data["topic_creation_config"]["retention.ms"] == str(7 * 1000 * 60 * 60 * 24)
