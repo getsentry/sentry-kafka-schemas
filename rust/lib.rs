ba6598f7acf61cb2c60e4c5281edcf72a0bd8317
Dan Fuller <dfuller@sentry.io>
2024-12-21 13:51:36 +1100

    tests

--- rust/lib.rs
+++ rust/lib.rs
@@ -238,7 +238,6 @@ mod tests {
         // Did not error
         get_schema("snuba-queries", Some(1)).unwrap();
         get_schema("transactions", Some(1)).unwrap();
-        get_schema("snuba-uptime-results", Some(1)).unwrap();
     }
 
     fn validate_schema(schema_name: &str) {
@@ -260,6 +259,5 @@ mod tests {
     fn test_validate() {
         validate_schema("snuba-queries");
         validate_schema("uptime-results");
-        validate_schema("snuba-uptime-results");
     }
 }
